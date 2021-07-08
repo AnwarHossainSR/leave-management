2ff29f364624784dc82a081a5ea3a23d89fd7fd8
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-08 08:31:55 +0600

    Setup Index Action & Controller

--- leave-management/Models/LeaveTypeVM.cs
+++ leave-management/Models/LeaveTypeVM.cs
@@ -12,6 +12,7 @@ namespace leave_management.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name="Date Created")]
         public DateTime DateCreated { get; set; }
     }
 
